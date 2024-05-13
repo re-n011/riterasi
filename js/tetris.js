cebc7d9b5fba325f0004cf9b9dd172341d708896
Ren Ogura <231205052@ccmailg.meijo-u.ac.jp>
2024-05-13 16:03:22 +0900

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -22,11 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-<<<<<<< HEAD
     'cyan', 'orange', 'skyblue', 'yellow', 'red', 'lime', 'purple'
-=======
-    'skyblue', 'orange', 'blue', 'yellow', 'red', 'lime', 'purple'
->>>>>>> 3cad12bf922e5779926cba8101dccfb62ec904cb
 ];
 
 // creates a new 4x4 shape in global variable 'current'
